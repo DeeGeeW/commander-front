ac945490ed15d9e470a9bbc0537bf98104915bf3
DeeGeeWest <DeeGeeWest@gmail.com>
2022-06-15 14:15:26 -0400

    im getting closer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,6 +55,11 @@ async function getDecks() {
 
     // 👇️ "response status is: 200"
     console.log('response status is: ');
+    var i = 0;
+    while (i < decks.length) {
+      return decks[i];
+      i++;
+    }
     return decks;
 
     return data;
